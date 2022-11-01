ff9ba034f4c15d2e4252f8d314520208891773f6
Vbeo147 <vbeo147@gmail.com>
2022-11-01 20:00:11 +0900

    rootReducer

--- src/main.jsx
+++ src/main.jsx
@@ -2,39 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { combineReducers, legacy_createStore as createStore } from "redux";
-import firebase from "firebase/compat/app";
-import "firebase/compat/auth";
-import "firebase/compat/firestore";
+import { legacy_createStore as createStore } from "redux";
 import App from "./components/App";
-import {
-  firebaseReducer,
-  ReactReduxFirebaseProvider,
-} from "react-redux-firebase";
-import { firestoreReducer, createFirestoreInstance } from "redux-firestore";
-
-const fbConfig = {
-  apiKey: "AIzaSyAkfoVPY7KHRfTONsgCSeoWDk4DisytmY0",
-  authDomain: "blog-4cd65.firebaseapp.com",
-  projectId: "blog-4cd65",
-  storageBucket: "blog-4cd65.appspot.com",
-  messagingSenderId: "801967607293",
-  appId: "1:801967607293:web:74aa1468b705af0f885d94",
-  measurementId: "G-MTC5L9T9ST",
-};
+import { ReactReduxFirebaseProvider } from "react-redux-firebase";
+import { createFirestoreInstance } from "redux-firestore";
+import { rootReducer } from "./rootReducer";
+import firebase from "./rootReducer";
 
 const rrfConfig = {
   userProfile: "users",
   useFirestoreForProfile: true,
 };
-
-firebase.initializeApp(fbConfig);
-firebase.firestore();
-
-const rootReducer = combineReducers({
-  firebase: firebaseReducer,
-  firestore: firestoreReducer,
-});
 
 const initialState = {};
 const store = createStore(rootReducer, initialState);
---
+++ src/rootReducer.js
@@ -0,0 +1,26 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+import { firebaseReducer } from "react-redux-firebase";
+import { firestoreReducer } from "redux-firestore";
+import { combineReducers } from "redux";
+
+const fbConfig = {
+  apiKey: "AIzaSyAkfoVPY7KHRfTONsgCSeoWDk4DisytmY0",
+  authDomain: "blog-4cd65.firebaseapp.com",
+  projectId: "blog-4cd65",
+  storageBucket: "blog-4cd65.appspot.com",
+  messagingSenderId: "801967607293",
+  appId: "1:801967607293:web:74aa1468b705af0f885d94",
+  measurementId: "G-MTC5L9T9ST",
+};
+
+firebase.initializeApp(fbConfig);
+firebase.firestore();
+
+export const rootReducer = combineReducers({
+  firebase: firebaseReducer,
+  firestore: firestoreReducer,
+});
+
+export default firebase;
