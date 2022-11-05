ce7ec52ea213062a1d040b66a64dc2150e9b73d4
Vbeo147 <vbeo147@gmail.com>
2022-11-05 09:48:03 +0900

    WritePage Form

--- src/routes/WritePage.jsx
+++ src/routes/WritePage.jsx
@@ -1,5 +1,45 @@
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+
 function WritePage() {
-  return <div>Write</div>;
+  const [title, setTitle] = useState("");
+  const [text, setText] = useState("");
+  const navigate = useNavigate();
+  const onTitleChange = (e) => {
+    setTitle(e.target.value);
+  };
+  const onTextChange = (data) => {
+    setText(data);
+  };
+  console.log(title, text);
+  return (
+    <div>
+      <form>
+        <div>
+          <input
+            onChange={onTitleChange}
+            type="text"
+            placeholder="제목"
+            required
+          />
+          <CKEditor
+            editor={ClassicEditor}
+            data=""
+            onChange={(_, editor) => {
+              const data = editor.getData();
+              onTextChange(data);
+            }}
+          />
+        </div>
+        <div>
+          <button>Enter</button>
+          <button onClick={() => navigate(-1)}>Close</button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default WritePage;
