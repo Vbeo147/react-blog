44f63891b7f0428b607440d7012a7719d7ee1be9
Vbeo147 <vbeo147@gmail.com>
2022-11-05 12:52:39 +0900

    TagMenu tagform

--- src/components/TagMenu.jsx
+++ src/components/TagMenu.jsx
@@ -1,5 +1,12 @@
+import { useState } from "react";
+import { useFirestore } from "react-redux-firebase";
+
 function TagMenu() {
-  const Tags = ["ReactJS", "Firebase"];
+  const [tag, setTag] = useState("");
+  const firestore = useFirestore();
+  const onChange = (e) => {
+    setTag(e.target.value);
+  };
   return (
     <div
       style={{
@@ -8,9 +15,21 @@ function TagMenu() {
         border: "1px solid black",
       }}
     >
-      {Tags.map((item, index) => (
-        <div key={index}>{item}</div>
-      ))}
+      <div>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            firestore.collection("tags").add({ tag });
+          }}
+        >
+          <input
+            onChange={onChange}
+            type="text"
+            placeholder="추가할 태그를 입력해주세요"
+          />
+          <button type="submit">+</button>
+        </form>
+      </div>
     </div>
   );
 }
--- src/routes/WritePage.jsx
+++ src/routes/WritePage.jsx
@@ -2,21 +2,29 @@ import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import { useFirestore } from "react-redux-firebase";
 
 function WritePage() {
   const [title, setTitle] = useState("");
   const [text, setText] = useState("");
   const navigate = useNavigate();
+  const firestore = useFirestore();
   const onTitleChange = (e) => {
     setTitle(e.target.value);
   };
   const onTextChange = (data) => {
     setText(data);
   };
-  console.log(title, text);
+  const onSubmit = (e) => {
+    e.preventDefault();
+    firestore.collection("test").add({
+      title: title,
+      text: text,
+    });
+  };
   return (
     <div>
-      <form>
+      <form onSubmit={onSubmit}>
         <div>
           <input
             onChange={onTitleChange}
@@ -27,6 +35,10 @@ function WritePage() {
           <CKEditor
             editor={ClassicEditor}
             data=""
+            config={{
+              placeholder: "내용을 입력하세요",
+              toolbar: ["bold", "italic"],
+            }}
             onChange={(_, editor) => {
               const data = editor.getData();
               onTextChange(data);
@@ -34,7 +46,7 @@ function WritePage() {
           />
         </div>
         <div>
-          <button>Enter</button>
+          <button type="submit">Enter</button>
           <button onClick={() => navigate(-1)}>Close</button>
         </div>
       </form>
