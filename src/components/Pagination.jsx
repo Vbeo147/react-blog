1e4e11566b20f2a5fe1e2bf7188ec91121ecd677
Vbeo147 <vbeo147@gmail.com>
2022-11-16 10:30:11 +0900

    [Code] Create many state

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -3,10 +3,15 @@ import { useState, useEffect } from "react";
 
 function Pagination({ itemsPerPage, items }) {
   const [currentPage, setCurrentPage] = useState(0);
+  const [PageCount, setPageCount] = useState(0);
   const [startIndex, setStartIndex] = useState(0);
   const [lastIndex, setLastIndex] = useState(3);
   const BtnLimit = 5;
-  useEffect(() => {}, []);
+  useEffect(() => {
+    if (items) {
+      setPageCount(Math.ceil(items.length / itemsPerPage));
+    }
+  }, [itemsPerPage, items]);
   return (
     <>
       <div>{/* Components CurrentList */}</div>
