b999cc8ae7db59368f433fb45dfdeaf690615620
Vbeo147 <vbeo147@gmail.com>
2022-11-16 09:48:17 +0900

    [Delete] React Pagination Module

--- src/routes/Pagination.jsx
+++ src/routes/Pagination.jsx
@@ -1,13 +1,9 @@
-import ReactPaginate from "react-paginate";
 import { useState, useEffect, useMemo, useRef } from "react";
 import { useFirestoreConnect } from "react-redux-firebase";
 import { useSelector } from "react-redux";
 import CurrentList from "../components/CurrentList";
 
 function Pagination() {
-  const [currentItems, setCurrentItems] = useState([]);
-  const [pageCount, setPageCount] = useState(0);
-  const [itemOffset, setItemOffset] = useState(0);
   const [sort, setSort] = useState(false);
   const [tagSearch, setTagSearch] = useState("");
   const inputRef = useRef();
@@ -30,18 +26,6 @@ function Pagination() {
         }),
     [writeSelector, sort, tagSearch]
   );
-  const itemsPerPage = 10;
-  useEffect(() => {
-    if (items) {
-      const endOffset = itemOffset + itemsPerPage;
-      setCurrentItems(items.slice(itemOffset, endOffset));
-      setPageCount(Math.ceil(items.length / itemsPerPage));
-    }
-  }, [itemOffset, itemsPerPage, items]);
-  const handlePageClick = (e) => {
-    const newOffset = (e.selected * itemsPerPage) % items.length;
-    setItemOffset(newOffset);
-  };
   const onChange = (e) => {
     setTagSearch(e.target.value);
   };
@@ -66,22 +50,6 @@ function Pagination() {
         <button onClick={() => setSort(!sort)}>
           {sort ? "오래된순" : "최신순"}
         </button>
-      </div>
-      <CurrentList currentItems={currentItems} />
-      <div>
-        {writeSelector ? (
-          <ReactPaginate
-            breakLabel="..."
-            nextLabel=">"
-            onPageChange={handlePageClick}
-            pageRangeDisplayed={3}
-            pageCount={pageCount}
-            previousLabel="<"
-            renderOnZeroPageCount={null}
-          />
-        ) : (
-          "게시글이 없습니다"
-        )}
       </div>
     </>
   );
