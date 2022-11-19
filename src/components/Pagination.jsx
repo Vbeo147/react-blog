a4ade476f87aa430cac302e33081cb01b063f503
Vbeo147 <vbeo147@gmail.com>
2022-11-19 16:25:16 +0900

    [Code] pagination finish but have bug

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 function Pagination({ itemsPerPage, items, currentPage, BtnLimit }) {
-  const [currentItems, setCurrentItems] = useState();
+  const [currentItems, setCurrentItems] = useState([]);
   const [pageCount, setPageCount] = useState(0);
   const [startBtnIndex, setStartBtnIndex] = useState(
     Math.floor(currentPage / BtnLimit) * BtnLimit
@@ -19,16 +19,15 @@ function Pagination({ itemsPerPage, items, currentPage, BtnLimit }) {
   const SaveStartIndex = items && BtnArr[0];
   const SaveLastIndex = items && BtnArr[BtnLimit - 1];
   useEffect(() => {
+    const isHome = currentPage < BtnLimit;
     if (items) {
       setPageCount(Math.ceil(items.length / itemsPerPage));
-      if (currentPage - 1 === SaveStartIndex) {
-        setStartBtnIndex((prev) =>
-          currentPage < BtnLimit ? 0 : prev - BtnLimit
-        );
-      } else if (currentPage - 1 === SaveLastIndex) {
-        setStartBtnIndex((prev) =>
-          currentPage < BtnLimit ? 0 : prev + BtnLimit
-        );
+      if (pageCount !== BtnLimit) {
+        if (SaveStartIndex === currentPage - 1) {
+          setStartBtnIndex((prev) => (isHome ? 0 : prev - BtnLimit));
+        } else if (SaveLastIndex === currentPage - 1) {
+          setStartBtnIndex((prev) => (isHome ? 0 : prev + BtnLimit));
+        }
       }
       setLastBtnIndex(
         startBtnIndex + BtnLimit > pageCount
@@ -36,12 +35,21 @@ function Pagination({ itemsPerPage, items, currentPage, BtnLimit }) {
           : startBtnIndex + BtnLimit
       );
     }
-  }, [items, itemsPerPage, currentPage, BtnLimit, startBtnIndex, BtnArr]);
+  }, [startBtnIndex, BtnArr, pageCount]);
+  useEffect(() => {
+    const itemOffset = (currentPage - 1) * itemsPerPage;
+    const endOffset = itemOffset + itemsPerPage;
+    if (items) {
+      setCurrentItems(items.slice(itemOffset, endOffset));
+    }
+  }, [currentPage, items]);
   return (
     <>
       {items && (
         <>
-          <div></div>
+          <div>
+            <CurrentList currentItems={currentItems} />
+          </div>
           <ul>
             {BtnArr.map((item, index) => {
               return (
