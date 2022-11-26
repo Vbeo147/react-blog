243a33004ea1dacb962fcae6c949f1d6bd8834df
Vbeo147 <vbeo147@gmail.com>
2022-11-26 16:34:07 +0900

    [Code] Add DetailPage createdAt

--- src/routes/DetailPage.jsx
+++ src/routes/DetailPage.jsx
@@ -8,22 +8,38 @@ function DetailPage() {
   useFirestoreConnect(["write"]);
   const writeSelector = useSelector((state) => state.firestore.data.write);
   if (writeSelector) {
+    const timestamp = new Intl.DateTimeFormat("ko-KR", {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
+    });
     return (
       <div className="main-padding md:w-[800px] 2xl:w-[1200px]">
         {writeSelector[id] ? (
           <>
             <div className="flex flex-col justify-start w-full">
-              <div className="flex justify-center items-center mb-10 border border-2 border-b-gray-400 pb-5 w-full select-none">
-                <span className="text-3xl mr-10 overflow-hidden">{`[ ${writeSelector[
-                  id
-                ].categoryName.slice(0, 15)} ]`}</span>
-                <span
-                  title={writeSelector[id].info.title}
-                  className="text-3xl overflow-hidden"
-                >
-                  {writeSelector[id].info.title.slice(0, 20)}
-                </span>
+              {/* first div */}
+              <div className="flex justify-center items-center mb-10 border border-2 border-b-gray-400 pb-3 w-full select-none">
+                <div className="flex flex-col justify-center w-full">
+                  <div className="flex flex-row justify-center items-center">
+                    <span className="text-3xl mr-10 overflow-hidden">{`[ ${writeSelector[
+                      id
+                    ].categoryName.slice(0, 15)} ]`}</span>
+                    <span
+                      title={writeSelector[id].info.title}
+                      className="text-3xl overflow-hidden"
+                    >
+                      {writeSelector[id].info.title.slice(0, 20)}
+                    </span>
+                  </div>
+                  <div className="flex flex-row justify-end items-center opacity-60">
+                    <span>{timestamp.format(writeSelector[id].createdAt)}</span>
+                  </div>
+                </div>
               </div>
+              {/* second div */}
               <div
                 className="break-all leading-7 font-mono"
                 dangerouslySetInnerHTML={{
