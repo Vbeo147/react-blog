f8d4637f3c0fe6ea1e91448d7e9e00c3eba9c448
Vbeo147 <vbeo147@gmail.com>
2022-12-02 18:48:03 +0900

    [Code] AboutPage ambody scroll page

--- src/routes/AboutPage.jsx
+++ src/routes/AboutPage.jsx
@@ -2,33 +2,35 @@ import { useEffect, useState, useRef } from "react";
 
 function AboutPage() {
   const [slide, setSlide] = useState(true);
-  const [page, setPage] = useState(1);
   const containerRef = useRef(null);
+  let page = 0;
   useEffect(() => {
     const onWheel = (e) => {
       e.preventDefault();
       const { deltaY } = e;
+      const lastPage = 3;
       if (slide) {
         setSlide(false);
-        if (deltaY < 0 && page > 0) {
-          setPage(page - 1);
-        } else if (deltaY > 0) {
-          setPage(page + 1);
+        if (deltaY < 0 && page !== 0) {
+          page--;
+        }
+        if (deltaY > 0 && page !== lastPage) {
+          page++;
         }
         window.scrollTo({
           top: window.innerHeight * page,
           behavior: "smooth",
         });
+        console.log(page);
         setSlide(true);
       }
-      console.log(page);
     };
     const containerRefCurrent = containerRef.current;
     containerRefCurrent.addEventListener("wheel", onWheel);
     return () => {
       containerRefCurrent.removeEventListener("wheel", onWheel);
     };
-  }, [page, slide]);
+  }, [slide, page]);
   return (
     <div className="main-padding" ref={containerRef}>
       <div className="inner">1</div>
