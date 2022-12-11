955271931cf3dde8fd9cf380e6ff8b5661abb319
Vbeo147 <vbeo147@gmail.com>
2022-12-11 17:51:59 +0900

    tagPage bug fix

--- src/routes/TagPage.jsx
+++ src/routes/TagPage.jsx
@@ -16,49 +16,49 @@ function TagPage() {
   );
   const writeSelector = useSelector((state) => state.firestore.data.write);
   const navigate = useNavigate();
-  return (
-    <>
-      <div className="main-padding w-full">
-        <div className="flex flex-wrap w-[800px] 2xl:w-[1200px] mb-20">
-          {categorySelector && (
-            <>
-              {Object.keys(categorySelector)
-                .filter((categoryName) => {
-                  return (
-                    categorySelector[categoryName]?.CheckUndefined !==
-                    (null || undefined)
-                  );
-                })
-                .map((item, index) => {
-                  return (
-                    <ToggleBtn
-                      onClick={() => {
-                        const isEmpty = !toggle.find(
-                          (toggles) => toggles === item
-                        );
-                        if (isEmpty) {
-                          setToggle((prev) => [...prev, item]);
-                        } else if (!isEmpty) {
-                          setToggle((prev) =>
-                            prev.filter((toggles) => toggles !== item)
+  if (categorySelector) {
+    return (
+      <>
+        <div className="main-padding w-full">
+          <div className="flex flex-wrap w-[800px] 2xl:w-[1200px] mb-20">
+            {categorySelector && (
+              <>
+                {Object.keys(categorySelector)
+                  .filter((categoryName) => {
+                    return (
+                      categorySelector[categoryName]?.CheckUndefined !==
+                      (null || undefined)
+                    );
+                  })
+                  .map((item, index) => {
+                    return (
+                      <ToggleBtn
+                        onClick={() => {
+                          const isEmpty = !toggle.find(
+                            (toggles) => toggles === item
                           );
-                        }
-                      }}
-                      background={toggle.find((toggles) => toggles === item)}
-                      className="mr-5 cursor-pointer select-none border border-2 border-gray-300 px-1.5 py-1 rounded-[5px] w-1/10"
-                      key={index}
-                    >
-                      {item}
-                    </ToggleBtn>
-                  );
-                })}
-            </>
-          )}
-        </div>
-        <div className="flex flex-col items-center justify-start w-full">
-          {writeSelector && (
-            <>
-              {toggle.map((item) =>
+                          if (isEmpty) {
+                            setToggle((prev) => [...prev, item]);
+                          } else if (!isEmpty) {
+                            setToggle((prev) =>
+                              prev.filter((toggles) => toggles !== item)
+                            );
+                          }
+                        }}
+                        background={toggle.find((toggles) => toggles === item)}
+                        className="mr-5 cursor-pointer select-none border border-2 border-gray-300 px-1.5 py-1 rounded-[5px] w-1/10"
+                        key={index}
+                      >
+                        {item}
+                      </ToggleBtn>
+                    );
+                  })}
+              </>
+            )}
+          </div>
+          <div className="flex flex-col items-center justify-start w-full">
+            {writeSelector &&
+              toggle.map((item) =>
                 Object.keys(writeSelector)
                   .map((id) => {
                     return { item: writeSelector[id], id: id };
@@ -85,12 +85,17 @@ function TagPage() {
                     }
                   })
               )}
-            </>
-          )}
+          </div>
         </div>
+      </>
+    );
+  } else {
+    return (
+      <div className="main-padding w-full flex justify-center">
+        태그가 존재하지 않습니다
       </div>
-    </>
-  );
+    );
+  }
 }
 
 export default TagPage;
